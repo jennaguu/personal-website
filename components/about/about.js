232089d6d9f95321073292958b3c95321abe4523
jennaguu <jenna.gu@ymail.com>
2023-07-31 15:12:11 -0400

    Refactor About page to allow for bullets and links

--- components/about/about.js
+++ components/about/about.js
@@ -6,11 +6,36 @@ import Image from 'next/image';
 const Section = ({ title, content }) => (
   <div className={styles.section}>
     <h2 className={styles.sectionTitle}>{title}</h2>
-    <p className={styles.sectionContent}>{content}</p>
+    <div className={styles.sectionContent}>
+      {typeof content === 'string' ? (
+        <p dangerouslySetInnerHTML={{ __html: content }} />
+        ) : (
+        <ul>
+          {content.map((point, index) => (
+            <li key={index}>{point}</li>
+          ))}
+        </ul>
+      )}
+    </div>
   </div>
 );
 
 const About = () => {
+  const introductionText = "Hi! My name is Jenna Gu, welcome to my website! Here are a few facts about me...";
+  const introductionPoints = [
+    'I have a twin sister who is currently studying at Rice University',
+    "I've figure skated for 13 years, and I'm part of Cornell's figure skating team (CUFSC)",
+    "One of my favorite things is traveling with my family, and my two favorite places that we've been to are Singapore and Florence, Italy!"
+  ];
+
+  const educationContent = [
+    'Rising junior at Cornell University studying computer and information science',
+    'Favorite courses include "Data Structures and Functional Programming", "Discrete Structures", and "Intermediate Chinese Reading and Writing"'
+  ];
+
+  const workExperienceContent = "I'm currently interning as a software developer at <a href='https://www.verisk.com'>Verisk</a>";
+
+
   return (
     <div className={styles.container}>
       <div className={styles.profilePicture}>
@@ -24,19 +49,10 @@ const About = () => {
         />
       </div>
       <h1 className={styles.title}>About Me</h1>
-      <Section
-        title="Introduction"
-        content="Hello! I am [Your Name], and this is my personal website. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget arcu euismod, vestibulum est at, laoreet eros."
-      />
-      <Section
-        title="Education"
-        content="I have completed my Bachelor's in Computer Science from [University Name]. Currently, I am pursuing a Master's degree in [Your Field of Study]."
-      />
-      <Section
-        title="Work Experience"
-        content="I have worked as a [Your Job Title] at [Company/Organization Name] for [Number of Years]. During this time, I was responsible for [Your Key Responsibilities]."
-      />
-      {/* Add more sections as needed */}
+      <Section title="Introduction" content={introductionText} />
+      <Section title="" content={introductionPoints} />
+      <Section title="Education" content={educationContent} />
+      <Section title="Work Experience" content={workExperienceContent} />
     </div>
   );
 };
